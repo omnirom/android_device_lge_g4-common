e572cb83761e5e1e69733b06969bfa0237ccf032
Jake Whatley <jacob.whatley91@gmail.com>
2016-05-24 19:46:34 +0200

    g4: kernel supports appended dtb
    Change-Id: I2540feed3503d33b493ea7e058117ac6453b17b0

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,39 +1,51 @@
+#
+# This makefile is not compatible with Yukon devices.
+#
+
 LOCAL_PATH := $(call my-dir)
 
-## Build and run dtbtool
-DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbTool.lge.msm8992$(HOST_EXECUTABLE_SUFFIX)
-INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
+uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk.cpio
+$(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
+	zcat $< > $@
 
-$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
-	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
-	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/
-	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
+$(INSTALLED_BOOTIMAGE_TARGET): \
+    $(PRODUCT_OUT)/kernel \
+    $(uncompressed_ramdisk) \
+    $(recovery_uncompressed_ramdisk) \
+    $(INSTALLED_RAMDISK_TARGET) \
+    $(PRODUCT_OUT)/utilities/toybox \
+    $(MKBOOTIMG) $(MINIGZIP) \
+    $(INTERNAL_BOOTIMAGE_FILES)
 
-
-## Overload bootimg generation: Same as the original, + --dt arg
-$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET) $(PRODUCT_OUT)/utilities/busybox
-	$(call pretty,"Target boot image: $@")
-	@echo -e ${CL_CYN}"----- Copying static busybox to ramdisk ------"${CL_RST}
-	$(hide) mkdir -p $(PRODUCT_OUT)/root/sbin/static
-	$(hide) cp $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/root/sbin/static/busybox
-	@echo -e ${CL_CYN}"----- Making boot ramdisk ------"${CL_RST}
-	$(hide) rm -f $(INSTALLED_RAMDISK_TARGET)
-	$(hide) $(MKBOOTFS) $(TARGET_ROOT_OUT) | $(MINIGZIP) > $(INSTALLED_RAMDISK_TARGET)
 	@echo -e ${CL_CYN}"----- Making boot image ------"${CL_RST}
-	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
-	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
+	$(hide) rm -fr $(PRODUCT_OUT)/combinedroot
+	$(hide) mkdir -p $(PRODUCT_OUT)/combinedroot/sbin
+
+	$(hide) cp $(uncompressed_ramdisk) $(PRODUCT_OUT)/combinedroot/sbin/
+	$(hide) cp -n $(uncompressed_ramdisk) $(recovery_uncompressed_ramdisk)
+	$(hide) cp $(recovery_uncompressed_ramdisk) $(PRODUCT_OUT)/combinedroot/sbin/
+	$(hide) cp $(PRODUCT_OUT)/utilities/toybox $(PRODUCT_OUT)/combinedroot/sbin/
+
+	$(hide) $(MKBOOTFS) $(PRODUCT_OUT)/combinedroot/ > $(PRODUCT_OUT)/combinedroot.cpio
+	$(hide) cat $(PRODUCT_OUT)/combinedroot.cpio | gzip > $(PRODUCT_OUT)/combinedroot.fs
+
+	$(hide) $(MKBOOTIMG) \
+        --kernel $(PRODUCT_OUT)/kernel \
+        --ramdisk $(PRODUCT_OUT)/combinedroot.fs \
+        --cmdline "$(BOARD_KERNEL_CMDLINE)" \
+        --base $(BOARD_KERNEL_BASE) \
+        --pagesize $(BOARD_KERNEL_PAGESIZE) \
+        -o $(INSTALLED_BOOTIMAGE_TARGET)
 	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
 
-## Overload recoveryimg generation: Same as the original, + --dt arg
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
-		$(recovery_uncompressed_ramdisk) \
-		$(recovery_kernel) \
-		$(MINIGZIP)
-	@echo -e ${CL_CYN}"----- Making recovery ramdisk ------"${CL_RST}
-	$(hide) rm -f $(recovery_uncompressed_ramdisk)
-	$(MKBOOTFS) $(TARGET_RECOVERY_ROOT_OUT) > $(recovery_uncompressed_ramdisk)
-	$(MINIGZIP) < $(recovery_uncompressed_ramdisk) > $(recovery_ramdisk)
+INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel)
 	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
-	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
-	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+	$(hide) $(MKBOOTIMG) \
+        --kernel $(PRODUCT_OUT)/kernel \
+        --ramdisk $(PRODUCT_OUT)/ramdisk-recovery.img \
+        --cmdline "$(BOARD_KERNEL_CMDLINE)" \
+        --base $(BOARD_KERNEL_BASE) \
+        --pagesize $(BOARD_KERNEL_PAGESIZE) \
+        -o $(INSTALLED_RECOVERYIMAGE_TARGET)
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
